7827e12d9efb446cf93e4a29c1f0fdc28aa7446e
david <david@adyoulike.com>
2018-01-26 18:09:17 +0100

    add CSV download

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -11,10 +11,10 @@ $(document).ready(function() {
 
   colorSettings = JSON.parse(colorSettings);
 
-  if(colorSettings.theme == "dark_theme") {
+  if(colorSettings && colorSettings.theme && colorSettings.theme == "dark_theme") {
     $("body").removeClass("light_theme").addClass("dark_theme");
   }
-  else if(colorSettings.theme == "light_theme") {
+  else if(colorSettings && colorSettings.theme && colorSettings.theme == "light_theme") {
     $("body").removeClass("dark_theme").addClass("light_theme");
   }
 
@@ -48,6 +48,103 @@ $(document).on("click", "#launch-semantic", function() {
       display_concepts(data.concepts);
       display_entities(data.entities);
       display_taxonomy(data.taxonomy);
+
+      var CSVdata = {};
+      CSVdata["emotions"] = [];
+      Object.keys(data.emotions).forEach(function(key) {
+        var s = [];
+        s.push(key);
+        s.push(percent(data.emotions[key], "%"));
+        CSVdata["emotions"].push(s);
+      })
+      CSVdata["iab"] = [];
+      data.taxonomy.forEach(function(obj) {
+        var a = [];
+        a.push(obj.label);
+        a.push(percent(obj.score, "%"));
+        CSVdata["iab"].push(a);
+      });
+      CSVdata["keywords"] = [];
+      data.keywords.forEach(function(obj) {
+        var o = [];
+        o.push(obj.text);
+        o.push(percent(obj.relevance, "%"));
+        CSVdata["keywords"].push(o);
+      });
+      CSVdata["concepts"] = [];
+      data.concepts.forEach(function(obj) {
+        var c = [];
+        c.push(obj.text);
+        c.push(percent(obj.relevance, "%"));
+        CSVdata["concepts"].push(c);
+      });
+      CSVdata["entities"] = [];
+      data.entities.forEach(function(obj) {
+        var e = [];
+        e.push(obj.text);
+        e.push(percent(obj.relevance, "%"));
+        CSVdata["entities"].push(e);
+      });
+
+
+      var pageURL = tab.url;
+      pageURL = pageURL.replace(/\?.*/g, "");
+
+      var analysisDate = new Date();
+      var dd = analysisDate.getDate();
+      var mM = analysisDate.getMonth()+1;
+      var yyyy = analysisDate.getFullYear();
+      if(dd < 10){
+          dd = "0" + dd;
+      }
+      if(mM < 10){
+          mM = "0" + mM;
+      }
+      var hh = analysisDate.getHours();
+      var mm = analysisDate.getMinutes();
+      var ss = analysisDate.getSeconds();
+      var analysisDate = mM + "-" + dd + "-" + yyyy + " at " + hh + ":" + mm + ":" + ss;
+
+      var JSONToCSV = function(data, url, date) {
+
+          var obj = typeof data != "object" ? JSON.parse(data) : data;
+          var CSVData = pageURL + " " + date + " \r\n";
+
+          for(keys in obj) {
+
+            CSVData += keys.toUpperCase() + "\r\n";
+
+            if(obj[keys].length) {
+              var _obj = obj[keys];
+              for(_keys in _obj) {
+                var line = JSON.stringify(_obj[_keys]);
+                line = line.replace(/\[/, "");
+                line = line.replace(/\]/, "");
+                line = line.replace(/[{}]/g, "");
+                line = line.replace(/[\:]/g, ",");
+                line = line + "\r\n";
+                CSVData += line;
+              }
+            }
+            else {
+              var line = JSON.stringify(obj[keys]);
+              line = line.replace(/\[/, "");
+              line = line.replace(/\]/, "");
+              line = line.replace(/[{}]/g, "");
+              line = line.replace(/[\:]/g, ",");
+              CSVData += line + "\r\n";
+            }
+          };
+
+          return CSVData;
+
+      }
+
+      $("#csv_download").closest("a").attr({
+        "style" : "text-decoration:none;",
+        "href": "data:text.csv;charset=utf-8," + JSONToCSV(CSVdata, pageURL, analysisDate),
+        "download" : "adyoulike_semantic.csv"
+      })
 
     })
       .done(function() {
